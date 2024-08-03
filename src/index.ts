16478af355c976260258869ec3c7adb3ab906cf2
Nathan <nathan.leroux3@gmail.com>
2024-08-03 22:12:35 +0200

    implemented apollo server + type graphql and dummy resolver

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
+import { ApolloServer } from "@apollo/server";
+import {expressMiddleware} from "@apollo/server/express4";
 import { AppDataSource, em } from "./data-source";
 import { Post } from "./entities/Post";
 import logger from "./utils/logger";
 import "dotenv/config";
 import express from "express"
+import { buildSchema } from "type-graphql";
+import { TestResolver } from "./resolvers/test";
+import cors from "cors";
 
-AppDataSource.initialize().then(() => {
+AppDataSource.initialize().then(async () => {
     logger.info(`Connection to database established on port ${process.env.DB_PORT}`);
-    AppDataSource.runMigrations(); 
+    await AppDataSource.runMigrations(); 
     // const myPost = em.create(Post, {title: "my first post"});
     // em.save(myPost);
-
+    
     const app = express();
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [TestResolver]
+        }),
+    });
+    await apolloServer.start();
+    app.use("/graphql", cors<cors.CorsRequest>(), express.json(), expressMiddleware(apolloServer, { context: async ({req}) => ({token: req.header.token})}));
     app.listen(process.env.APP_PORT, () => {
         logger.info(`App is running: http://localhost:${process.env.APP_PORT}`);
     })
